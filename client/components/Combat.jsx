f8f3921433c42ddf52ce6c664307b3668d5b819e
jordan-trimarchi <ajordant47@gmail.com>
2021-09-22 09:00:30 -0600

    working on calc

--- client/components/Combat.jsx
+++ client/components/Combat.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import unitStats from './unitStats.jsx';
 
 const Combat = () => {
@@ -6,6 +6,13 @@ const Combat = () => {
   const [targetUnit, setTargetUnit] = useState(null);
   const [targetUnitSaveAug, setTargetUnitSaveAug] = useState(0);
   const [rollToHitAug, setRollToHitAug] = useState(0);
+  const [isInPartialCover, setIsInPartialCover] = useState(false);
+  const [isNearCaptain, setIsNearCaptain] = useState(false);
+  const [isNearSergeant, setIsNearSergeant] = useState(false);
+  const [rollToHit, setRollToHit] = useState('');
+  const [distance, setDistance] = useState(0);
+
+
 
   const handleSaveAugChange = (event, value) => {
     if (event.target.checked) {
@@ -22,6 +29,25 @@ const Combat = () => {
     }
   };
 
+  const handleRollToHitCalc = () => {
+    if (distance) {
+      console.log(distance, unitStats[playerUnit]['Inches To Roll']);
+      setRollToHit(Math.round(distance * unitStats[playerUnit]['Inches To Roll']) + rollToHitAug);
+    }
+  };
+
+  useEffect(() => {
+    setIsNearCaptain(false);
+    setIsNearSergeant(false);
+    setIsInPartialCover(false);
+  }, [targetUnit]);
+
+  useEffect(() => {
+    handleRollToHitCalc();
+  }, [distance, rollToHitAug, playerUnit, targetUnit]);
+
+
+
   return (
     <>
       <select onChange={(event) => { setPlayerUnit(event.target.value); }} name="Unit">
@@ -32,7 +58,7 @@ const Combat = () => {
       </select>
       <select onChange={(event) => {
         setTargetUnit(event.target.value);
-        setTargetUnitSaveAug(unitStats[event.target.value]["Save Requirement"]);
+        setTargetUnitSaveAug(unitStats[event.target.value]["Save Requirement"] || 0);
       }} name="Target">
         <option value="">--Select your target--</option>
         {Object.keys(unitStats).map((unit) => {
@@ -41,33 +67,74 @@ const Combat = () => {
       </select>
       <div style={{ display: 'flex', flexDirection: 'row' }}>
         <div style={{ width: '50vw', display: 'flex', flexDirection: 'column', borderRight: 'solid' }}>
-          <h3>Player Unit: {playerUnit}</h3>
-          {playerUnit ? Object.keys(unitStats[playerUnit]).map((item) => {
-            return <div style={{ borderBottom: 'solid' }}>
-              <span className="info-point">{item}: </span>
-              <span>{unitStats[playerUnit][item]} </span>
+          {playerUnit ?
+            <div>
+              <h3>Player Unit: {playerUnit}</h3>
+              {Object.keys(unitStats[playerUnit]).map((item) => {
+                return <div style={{ borderBottom: 'solid' }}>
+                  <span className="info-point">{item}: </span>
+                  <span>{unitStats[playerUnit][item]} </span>
+                </div>
+              })}
+              {playerUnit && targetUnit ?
+                <div>
+                  <input onChange={(event) => {
+                    setDistance(event.target.value);
+                  }} type="number" step=".0625" placeholder='Inches' min="0" style={{ width: '5em' }} />
+                  {
+                    rollToHit > 20.5
+                      ? <div>Unable to hit.</div>
+                      : rollToHit
+                        ? <div>Roll of {rollToHit} required to hit.</div>
+                        : null
+                  }
+                  {targetUnit !== 'Captain' ? <div>
+                    <span style={{ borderBottom: 'solid' }} className="info-point">{`Within 'Call to Arms' radius (Template #3) of Captain:`}</span>
+                    <input type="checkbox" onChange={(event) => {
+                      handleRollToHitAugChange(event, -2);
+                      event.target.checked ? setIsNearCaptain(true) : setIsNearCaptain(false);
+                    }} checked={isNearCaptain} />
+                  </div> : null}
+                  {targetUnit !== 'Sergeant' ? <div>
+                    <span style={{ borderBottom: 'solid' }} className="info-point">{`Within 'Rally' radius (Template #2) of Sergeant:`}</span>
+                    <input type="checkbox" onChange={(event) => {
+                      handleRollToHitAugChange(event, -1);
+                      event.target.checked ? setIsNearSergeant(true) : setIsNearSergeant(false);
+                    }} checked={isNearSergeant} />
+                  </div> : null}
+                </div>
+                : null}
             </div>
-          }) : null}
+            : null}
         </div>
         <div style={{ width: '49vw', marginLeft: '1vw', display: 'flex', flexDirection: 'column', borderLeft: 'solid' }}>
-          <h3>Target Unit: {targetUnit}</h3>
           {targetUnit
             ?
             <div>
+              <h3>Target Unit: {targetUnit}</h3>
               <span style={{ borderBottom: 'solid' }} className="info-point">Inherant Save Requirement Augmentation: </span>
               <span> {unitStats[targetUnit]["Save Requirement"] || 0}</span>
               <div>
                 <span style={{ borderBottom: 'solid' }} className="info-point">In partial cover:</span>
-                <input type="checkbox" onChange={(event) => { handleRollToHitAugChange(event, 3) }} />
+                <input type="checkbox" onChange={(event) => {
+                  handleRollToHitAugChange(event, 3);
+                  event.target.checked ? setIsInPartialCover(true) : setIsInPartialCover(false);
+                }} checked={isInPartialCover} />
               </div>
-              <div>
+              {targetUnit !== 'Captain' ? <div>
                 <span style={{ borderBottom: 'solid' }} className="info-point">{`Within 'Call to Arms' radius (Template #3) of Captain:`}</span>
-                <input type="checkbox" onChange={(event) => { handleSaveAugChange(event, -2) }} />
-              </div>
-              <div>
-                <span style={{ borderBottom: 'solid' }} className="info-point">{`Within 'Rally' radius (Template $2) of Sergeant:`}</span>
-                <input type="checkbox" onChange={(event) => { handleSaveAugChange(event, -1) }} />
-              </div>
+                <input type="checkbox" onChange={(event) => {
+                  handleSaveAugChange(event, -2);
+                  event.target.checked ? setIsNearCaptain(true) : setIsNearCaptain(false);
+                }} checked={isNearCaptain} />
+              </div> : null}
+              {targetUnit !== 'Sergeant' ? <div>
+                <span style={{ borderBottom: 'solid' }} className="info-point">{`Within 'Rally' radius (Template #2) of Sergeant:`}</span>
+                <input type="checkbox" onChange={(event) => {
+                  handleSaveAugChange(event, -1);
+                  event.target.checked ? setIsNearSergeant(true) : setIsNearSergeant(false);
+                }} checked={isNearSergeant} />
+              </div> : null}
               <span className="info-point" style={{ borderBottom: 'solid' }}>Total Save Requirement Augmentation: </span>
               <span> {targetUnitSaveAug || 0}</span>
             </div>
