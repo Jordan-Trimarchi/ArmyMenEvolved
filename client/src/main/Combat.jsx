33c6333af8db13ad1dfc6297a2148b1a34c51ab1
jordan-trimarchi <ajordant47@gmail.com>
2021-10-13 16:25:26 -0600

    moves static displays for barrier and ground to targetUnitViews

--- client/src/main/Combat.jsx
+++ client/src/main/Combat.jsx
@@ -22,6 +22,8 @@ const Combat = () => {
   const [isCriticalHit, setIsCriticalHit] = useState(false);
   const [usingSideArm, setUsingSideArm] = useState(false);
 
+  const isBazooka = playerUnit && playerUnit.name === 'Bazooka';
+
   const handleRollToHitAugChange = (event, value) => {
     if (event.target.checked) {
       setRollToHitAug(rollToHitAug + value);
@@ -47,9 +49,9 @@ const Combat = () => {
 
   useEffect(() => {
     handleRollToHitCalc();
-    if (playerUnit && playerUnit.name === 'Bazooka' && targetUnit === 'barrier-vehicle') {
+    if (isBazooka && targetUnit === 'barrier-vehicle') {
       setCurrentITR(playerUnit['Inches To Roll vs Vehicle/Structure']);
-    } else if (playerUnit && playerUnit.name === 'Bazooka' && targetUnit !== 'barrier-vehicle') {
+    } else if (isBazooka && targetUnit !== 'barrier-vehicle') {
       setCurrentITR(playerUnit['Inches To Roll vs Infantry']);
     }
   }, [distance, rollToHitAug, playerUnit, targetUnit, elevation, currentITR]);
@@ -100,7 +102,7 @@ const Combat = () => {
               || unitStats[event.target.value]['Sidearm Inches To Roll'],
             );
           }}
-          name="Unit"
+          name="UnitSelect"
         >
           <option value="">--Select Unit--</option>
           {Object.keys(unitStats).map((unit) => <option value={unit} key={unit}>{unit}</option>)}
@@ -109,52 +111,21 @@ const Combat = () => {
           onChange={(event) => {
             setTargetUnit(unitStats[event.target.value] || event.target.value);
           }}
-          name="Target"
+          name="TargetSelect"
         >
           <option value="">--Select Target--</option>
-          {playerUnit && playerUnit.name === 'Bazooka' ? <option value="barrier-vehicle">Barrier / Vehicle</option> : null}
+          {isBazooka ? <option value="barrier-vehicle">Barrier / Vehicle</option> : null}
           {Object.keys(unitStats).map((unit) => <option value={unit} key={unit}>{unit}</option>)}
           <option value="ground">The Ground</option>
         </select>
       </div>
       <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
         <div>
-          {playerUnit
-            ? (
-              <PlayerUnitView />
-            )
-            : null}
+
+          {playerUnit ? (<PlayerUnitView />) : null}
         </div>
         <div>
-          {targetUnit && targetUnit !== 'barrier-vehicle' && targetUnit !== 'ground'
-            ? (
-              <TargetUnitView />
-            )
-            : null}
-          {targetUnit === 'barrier-vehicle'
-            ? (
-              <div style={{
-                transition: '1s', width: '38.5vw', display: 'flex', flexDirection: 'column', borderRight: 'solid', borderLeft: 'solid', borderBottom: 'solid',
-              }}
-              >
-                <h2 style={{ textDecoration: 'underline', display: 'flex', justifyContent: 'center' }}>Barrier / Vehicle</h2>
-                <div className="row">
-                  <span className="info-point">Suggested Tactics:</span>
-                  <span> Obliterate It. </span>
-                </div>
-              </div>
-            )
-            : null}
-          {targetUnit === 'ground'
-            ? (
-              <div style={{
-                transition: '1s', width: '38.5vw', display: 'flex', flexDirection: 'column', borderRight: 'solid', borderLeft: 'solid', borderBottom: 'solid',
-              }}
-              >
-                <h2 style={{ textDecoration: 'underline', display: 'flex', justifyContent: 'center' }}>The Ground</h2>
-                <h3 style={{ display: 'flex', justifyContent: 'center' }}>{'It\'s not the fall that kills you, it\'s the sudden stop at the end.'}</h3>
-              </div>
-            ) : null}
+          {targetUnit ? <TargetUnitView /> : null}
         </div>
       </div>
     </Context.Provider>
--- client/src/main/TargetUnitView.jsx
+++ client/src/main/TargetUnitView.jsx
@@ -31,41 +31,69 @@ const TargetUnitView = () => {
   };
 
   return (
-    <div
-      className="fade-in"
-      style={{
-        transition: '1s', width: '38.5vw', display: 'flex', flexDirection: 'column', borderLeft: 'solid', borderRight: 'solid',
-      }}
-    >
-      <h2 style={{ textDecoration: 'underline', display: 'flex', justifyContent: 'center' }}>
-        {`Target: ${targetUnit.name}`}
-      </h2>
-      <TargetUnitRows
-        targetUnit={targetUnit}
-        playerUnit={playerUnit}
-        targetUnitSaveAug={targetUnitSaveAug}
-        usingGrenade={usingGrenade}
-        usingSideArm={usingSideArm}
-        handleSaveAugChange={handleSaveAugChange}
-        isBehindDestBarrier={isBehindDestBarrier}
-        setIsBehindDestBarrier={setIsBehindDestBarrier}
-        isNearCaptain={isNearCaptain}
-        setIsNearCaptain={setIsNearCaptain}
-        isNearSergeant={isNearSergeant}
-        setIsNearSergeant={setIsNearSergeant}
-      />
-      <RollToSave
-        targetUnitSaveAug={targetUnitSaveAug}
-        rollResult={rollResult}
-        playerUnit={playerUnit}
-        usingGrenade={usingGrenade}
-        usingSideArm={usingSideArm}
-        rollToHit={rollToHit}
-        isCriticalHit={isCriticalHit}
-        usingMortarMechanics={usingMortarMechanics}
-        targetUnit={targetUnit}
-      />
-    </div>
+    <>
+      {targetUnit === 'barrier-vehicle'
+        ? (
+          <div style={{
+            transition: '1s', width: '38.5vw', display: 'flex', flexDirection: 'column', borderRight: 'solid', borderLeft: 'solid', borderBottom: 'solid',
+          }}
+          >
+            <h2 style={{ textDecoration: 'underline', display: 'flex', justifyContent: 'center' }}>Barrier / Vehicle</h2>
+            <h3 style={{ display: 'flex', justifyContent: 'center' }}>Obliterate it.</h3>
+          </div>
+        )
+        : null}
+
+      {targetUnit === 'ground'
+        ? (
+          <div style={{
+            transition: '1s', width: '38.5vw', display: 'flex', flexDirection: 'column', borderRight: 'solid', borderLeft: 'solid', borderBottom: 'solid',
+          }}
+          >
+            <h2 style={{ textDecoration: 'underline', display: 'flex', justifyContent: 'center' }}>The Ground</h2>
+            <h3 style={{ display: 'flex', justifyContent: 'center' }}>{'It\'s not the fall that kills you, it\'s the sudden stop at the end.'}</h3>
+          </div>
+        ) : null}
+      {targetUnit !== 'ground' && targetUnit !== 'barrier-vehicle'
+        ? (
+          <div
+            className="fade-in"
+            style={{
+              transition: '1s', width: '38.5vw', display: 'flex', flexDirection: 'column', borderLeft: 'solid', borderRight: 'solid',
+            }}
+          >
+            <h2 style={{ textDecoration: 'underline', display: 'flex', justifyContent: 'center' }}>
+              {`Target: ${targetUnit.name}`}
+            </h2>
+            <TargetUnitRows
+              targetUnit={targetUnit}
+              playerUnit={playerUnit}
+              targetUnitSaveAug={targetUnitSaveAug}
+              usingGrenade={usingGrenade}
+              usingSideArm={usingSideArm}
+              handleSaveAugChange={handleSaveAugChange}
+              isBehindDestBarrier={isBehindDestBarrier}
+              setIsBehindDestBarrier={setIsBehindDestBarrier}
+              isNearCaptain={isNearCaptain}
+              setIsNearCaptain={setIsNearCaptain}
+              isNearSergeant={isNearSergeant}
+              setIsNearSergeant={setIsNearSergeant}
+            />
+            <RollToSave
+              targetUnitSaveAug={targetUnitSaveAug}
+              rollResult={rollResult}
+              playerUnit={playerUnit}
+              usingGrenade={usingGrenade}
+              usingSideArm={usingSideArm}
+              rollToHit={rollToHit}
+              isCriticalHit={isCriticalHit}
+              usingMortarMechanics={usingMortarMechanics}
+              targetUnit={targetUnit}
+            />
+          </div>
+        )
+        : null}
+    </>
   );
 };
 
