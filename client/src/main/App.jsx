a01730adf1199f9d5d3e993d1e1dde65b360a26e
jordan-trimarchi <ajordant47@gmail.com>
2021-10-15 10:25:40 -0600

    makes rules button change to 'back' when viewing rules

--- client/src/main/App.jsx
+++ client/src/main/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div>
       <div className="header">
         <h1>Army Men Evolved</h1>
-        <input value="Rules" style={{ backgroundColor: 'rgb(100,100, 100, .75)' }} type="button" onClick={() => setViewingCombat(!viewingCombat)} />
+        <input value={viewingCombat ? 'Rules' : 'Back'} type="button" onClick={() => setViewingCombat(!viewingCombat)} />
       </div>
       {viewingCombat ? <Combat /> : <iframe title="rules" style={{ width: '100%', height: '100vh' }} src="https://drive.google.com/file/d/1AzVqaFvFOD8iXwt936GSCgFjrIP5iq-b/preview" />}
     </div>
